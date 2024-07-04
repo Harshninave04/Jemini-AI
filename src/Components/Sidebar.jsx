3e426a7770aa576bcf2c1780b33dd4204fc869fd
Harsh Ninave <harshninave58@gmail.com>
2024-07-04 23:13:12 +0530

    Sidebar Updated [40% similar]

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,22 +1,55 @@
 'use client';
 import React, { useState } from 'react';
-import { Menu, Plus } from 'lucide-react';
+import { Menu, Plus, CircleHelp, Settings, History } from 'lucide-react';
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="min-h-[100vh] inline-flex flex-col justify-between bg-bgSecondaryColor py-6 px-4">
-      <div>
+      <div className="">
         <Menu
           strokeWidth={1.5}
           size={25}
           onClick={() => setIsOpen(!isOpen)}
-          className="cursor-pointer text-softTextColor"
+          className="cursor-pointer text-softTextColor ml-[6px]"
         />
-        <div className="mt-14 text-sm inline-flex items-center gap-4 border rounded-full px-3 py-[9px] bg-gray-200 text-gray-700 cursor-pointer">
+        <div className="mt-14 text-sm inline-flex items-center gap-[14px] border rounded-full px-[8px] py-[7px] bg-gray-200 text-gray-700 cursor-pointer">
           <Plus size={20} className="" />
           {isOpen ? <p className="font-medium">New chat</p> : null}
         </div>
-        <p className="mt-8 text-sm cursor-pointer text-softTextColor font-semibold">Recent</p>
+        {isOpen ? (
+          <div className="flex flex-col">
+            <p className="mt-8 text-sm cursor-pointer font-semibold">Recent</p>
+          </div>
+        ) : null}
+      </div>
+      <div className="gap-6 flex flex-col">
+        <div className="flex flex-col gap-4 text-sm">
+          <div className="flex cursor-pointer items-center gap-[16px] pr-2.5">
+            <CircleHelp size={20} className="text-softTextColor" />
+            {isOpen ? (
+              <p className="cursor-pointer font-semibold text-softTextColor">Help</p>
+            ) : null}
+          </div>
+          <div className="flex cursor-pointer items-center gap-[16px] pr-2.5">
+            <History size={20} className="text-softTextColor" />
+            {isOpen ? (
+              <p className="cursor-pointer font-semibold text-softTextColor">History</p>
+            ) : null}
+          </div>
+          <div className="flex cursor-pointer items-center gap-[16px] pr-2.5">
+            <Settings size={20} className="text-softTextColor" />
+            {isOpen ? (
+              <p className="cursor-pointer font-semibold text-softTextColor">Settings</p>
+            ) : null}
+          </div>
+        </div>
+        {isOpen ? (
+          <div className="flex flex-col text-sm w-64">
+            <p className="text-softTextColor text-wrap text-xs pl-5">
+              Sitabuldi, Nagpur, Maharashtra, India Based on your places (home) • Update location
+            </p>
+          </div>
+        ) : null}
       </div>
     </div>
   );
