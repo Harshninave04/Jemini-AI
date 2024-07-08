70e268c544e24eacb7a67f0e646c7e01a7e0b23e
Harsh Ninave <harshninave58@gmail.com>
2024-07-08 21:13:46 +0530

    Gemini sample body

--- src/Components/GeminiBody.jsx
+++ src/Components/GeminiBody.jsx
@@ -1,88 +1,55 @@
+import { CircleUserRound } from 'lucide-react';
 import React from 'react';
 
 const GeminiBody = () => {
   return (
     <>
-      <div className="flex flex-col gap-5  p-16 ml-28 mt-6">
-        <div className="text-5xl font-semibold">
-          <h1>Hello, Harsh</h1>
-          <h1 className="text-softTextColor ">How can I help you today?</h1>
+      <div className="flex-1 min-h-[100vh] pb-[15vh] relative">
+        <div className="flex items-center justify-between p-4 text-gray-400 text-xl">
+          <p className="text-transparent bg-clip-text hover:bg-clip-text bg-gradient-to-r from-purple-300 to-pink-300 hover:bg-gradient-to-r hover:from-blue-400 hover:to-yellow-50 font-bold cursor-pointer">
+            JEMINI
+          </p>
+
+          <CircleUserRound size={40} className="text-softTextColor" />
         </div>
-        <div className="flex items-center mt-10">
-          <div className="max-w-80">
-            <a href="#" className="block rounded-lg p-4 shadow-sm shadow-indigo-100">
-              <img
-                alt=""
-                src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                className="h-56 w-full rounded-md object-cover"
-              />
-
-              <div className="mt-2">
-                <dl>
-                  <div>
-                    <dt className="sr-only">Price</dt>
-
-                    <dd className="text-sm text-gray-500">$240,000</dd>
-                  </div>
-
-                  <div>
-                    <dt className="sr-only">Address</dt>
-
-                    <dd className="font-medium">123 Wallaby Avenue, Park Road</dd>
-                  </div>
-                </dl>
-              </div>
-            </a>
+        <div className="max-w-[900px] m-auto">
+          <div className="my-8 font-medium p-5 leading-tight">
+            <p>
+              <span className="text-6xl font-bold text-transparent bg-clip-text hover:bg-clip-text bg-gradient-to-r from-blue-400 to-pink-500 hover:bg-gradient-to-r hover:from-blue-400 hover:to-yellow-50 cursor-not-allowed">
+                Hello, Harsh
+              </span>
+            </p>
+            <p className="text-[55px] -tracking-wider text-transparent bg-clip-text hover:bg-clip-text bg-gradient-to-r from-blue-400 to-yellow-50 hover:bg-gradient-to-r hover:from-purple-300 hover:to-pink-300 cursor-none">
+              What's on your mind today?
+            </p>
           </div>
-          <div className="max-w-80">
-            <a href="#" className="block rounded-lg p-4 shadow-sm shadow-indigo-100">
-              <img
-                alt=""
-                src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                className="h-56 w-full rounded-md object-cover"
-              />
-
-              <div className="mt-2">
-                <dl>
-                  <div>
-                    <dt className="sr-only">Price</dt>
-
-                    <dd className="text-sm text-gray-500">$240,000</dd>
-                  </div>
-
-                  <div>
-                    <dt className="sr-only">Address</dt>
-
-                    <dd className="font-medium">123 Wallaby Avenue, Park Road</dd>
-                  </div>
-                </dl>
-              </div>
-            </a>
-          </div>
-          <div className="max-w-80">
-            <a href="#" className="block rounded-lg p-4 shadow-sm shadow-indigo-100">
-              <img
-                alt=""
-                src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                className="h-56 w-full rounded-md object-cover"
-              />
-
-              <div className="mt-2">
-                <dl>
-                  <div>
-                    <dt className="sr-only">Price</dt>
-
-                    <dd className="text-sm text-gray-500">$240,000</dd>
-                  </div>
-
-                  <div>
-                    <dt className="sr-only">Address</dt>
-
-                    <dd className="font-medium">123 Wallaby Avenue, Park Road</dd>
-                  </div>
-                </dl>
-              </div>
-            </a>
+          {/* Cards */}
+          <div class="mx-8 my-14 grid grid-cols-2 gap-4">
+            {' '}
+            <div class="w-80 h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold">
+              <p class="px-5 py-2 text-gray-700 text-base">
+                Write a thank you note
+                <p className="text-gray-400 text-sm">to my boss</p>
+              </p>
+            </div>
+            <div class="w-80 h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold">
+              <p class="px-5 py-2 text-gray-700 text-base">
+                Write a thank you note
+                <p className="text-gray-400 text-sm">to my boss</p>
+              </p>
+            </div>
+            <div class="w-80 h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold">
+              <p class="px-5 py-2 text-gray-700 text-base">
+                Write a thank you note
+                <p className="text-gray-400 text-sm">to my boss</p>
+              </p>
+            </div>
+            <div class="w-80 h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold">
+              <p class="px-5 py-2 text-gray-700 text-base">
+                Write a thank you note
+                <p className="text-gray-400 text-sm">to my boss</p>
+              </p>
+            </div>
           </div>
         </div>
       </div>
