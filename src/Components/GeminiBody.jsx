676ec945b45c8291de5caa447b5097af8d9d33e9
Harsh Ninave <harshninave58@gmail.com>
2024-07-08 21:41:37 +0530

    Played with animations

--- src/Components/GeminiBody.jsx
+++ src/Components/GeminiBody.jsx
@@ -6,11 +6,11 @@ const GeminiBody = () => {
     <>
       <div className="flex-1 min-h-[100vh] pb-[15vh] relative">
         <div className="flex items-center justify-between p-4 text-gray-400 text-xl">
-          <p className="text-transparent bg-clip-text hover:bg-clip-text bg-gradient-to-r from-purple-300 to-pink-300 hover:bg-gradient-to-r hover:from-blue-400 hover:to-yellow-50 font-bold cursor-pointer">
+          <p className="text-transparent bg-clip-text hover:bg-clip-text bg-gradient-to-r from-purple-300 to-pink-300 hover:bg-gradient-to-r hover:from-blue-400 hover:to-yellow-50 font-bold cursor-pointer animate-bounce hover:animate-none">
             JEMINI
           </p>
 
-          <CircleUserRound size={40} className="text-softTextColor" />
+          <CircleUserRound size={40} className="text-softTextColor animate-spin cursor-pointer hover:animate-none" />
         </div>
         <div className="max-w-[900px] m-auto">
           <div className="my-8 font-medium p-5 leading-tight">
@@ -24,27 +24,27 @@ const GeminiBody = () => {
             </p>
           </div>
           {/* Cards */}
-          <div class="mx-8 my-14 grid grid-cols-2 gap-4">
+          <div class="mx-8 my-14 grid grid-cols-1 md:grid md:grid-cols-2 gap-4 ">
             {' '}
-            <div class="w-80 h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold">
+            <div class=" h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold bg-gradient-to-l from-yellow-50 to-orange-50 animate-pulse">
+              <p class="px-5 py-2 text-gray-800 text-base">
+                Write a thank you note
+                <p className="text-gray-400 text-sm">to my boss</p>
+              </p>
+            </div>
+            <div class=" h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold bg-gradient-to-r from-yellow-50 to-orange-50 animate-pulse">
               <p class="px-5 py-2 text-gray-700 text-base">
                 Write a thank you note
                 <p className="text-gray-400 text-sm">to my boss</p>
               </p>
             </div>
-            <div class="w-80 h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold">
+            <div class=" h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold bg-gradient-to-b from-yellow-50 to-orange-50 animate-pulse">
               <p class="px-5 py-2 text-gray-700 text-base">
                 Write a thank you note
                 <p className="text-gray-400 text-sm">to my boss</p>
               </p>
             </div>
-            <div class="w-80 h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold">
-              <p class="px-5 py-2 text-gray-700 text-base">
-                Write a thank you note
-                <p className="text-gray-400 text-sm">to my boss</p>
-              </p>
-            </div>
-            <div class="w-80 h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold">
+            <div class=" h-16 border-gray-100 bg-gray-200 rounded-xl shadow-md overflow-hidden font-bold bg-gradient-to-t from-yellow-50 to-orange-50 animate-pulse">
               <p class="px-5 py-2 text-gray-700 text-base">
                 Write a thank you note
                 <p className="text-gray-400 text-sm">to my boss</p>
